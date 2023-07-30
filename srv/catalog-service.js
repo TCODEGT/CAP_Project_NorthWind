0c7a017df3d5aa4fe6788cfe1cfcfdf021432e51
Vicente Clemente Rodríguez <vicente.clementerodriguez@telefonica.com>
2023-07-30 08:56:36 +0000

    add logs

--- srv/catalog-service.js
+++ srv/catalog-service.js
@@ -1,9 +1,16 @@
 const cds = require('@sap/cds');
 
-module.exports = cds.service.impl(async function() {
+module.exports = cds.service.impl(async function () {
 	const { Products } = this.entities;
 	const service = await cds.connect.to('NorthWind');
 	this.on('READ', Products, request => {
-	//	return service.tx(request).run(request.query);
+
+		const LOG = cds.log('sql');
+		LOG.info ('LOG.info: : Products - Read');
+		LOG.error ('LOG.error: : Products - Read');
+
+		console.log('CONSOLE.log : Products - Read');
+		return service.tx(request).run(request.query);
+		
 	});
 });
